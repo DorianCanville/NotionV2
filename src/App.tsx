fd4b9b64f1ae7df615d1cfd3263807389f8a9dec
Karl Duponchel <karlduponchel@MacBook-Pro-de-Karl.local>
2022-10-27 10:42:02 +0200

    template

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+import './styles/index.scss';
 
 function App() {
   return (
     <div className="App">
+      <div className='navbar'>
+        <img className='logo_cesi' src='./logo_cesi.png'/>
+      </div>
+      <div className='wrapper'>
+        <div className='worksheet'>
 
+        </div>
+        <div className='toolbox'>
+        </div>
+      </div>
     </div>
   );
 }
